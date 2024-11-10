b66a4592719ec2ca4b6e3da155304640c7a55de5
irynalomako <lomakoiryna86@gmail.com>
2024-11-10 15:20:35 +0200

    add: tasks to lesson 1

---
+++ Lesson 1/6Qb97gsv.js
@@ -0,0 +1,8 @@
+//Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+let firstName = 'Ірина';
+let middleName = 'Сергіївна';
+let lastName = 'Ломако';
+let person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person);
+let person1 = `${firstName} ${middleName} ${lastName}`;
+console.log(person1);
---
+++ Lesson 1/dYQNrBV.js
@@ -0,0 +1,28 @@
+//Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// Вивести кожну змінну за допомогою: console.log
+let a = 'hello';
+console.log (a);
+let b = 'owu';
+console.log(b);
+let c = "com";
+console.log(c);
+let d = "ua";
+console.log(d);
+let e = 1;
+console.log(e);
+let f = 10;
+console.log(f);
+let g = -999;
+console.log(g);
+let h = 123;
+console.log(h);
+const PI = 3.14;
+console.log(PI);
+let i = 2.7;
+console.log(i);
+let j = 16;
+console.log(j);
+let k = true;
+console.log(k);
+let l = false;
+console.log(l);
